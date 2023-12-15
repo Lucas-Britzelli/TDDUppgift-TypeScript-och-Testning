0be9c8cc84d6e14385e381352ff2af903fbb826c
Lucas <l.britzelli@gmail.com>
2023-12-15 14:39:28 +0100

    wrote function for lowercase function and changed the way I test my PrimeNumbers

--- Tests/isPrimeNumber.test.ts
+++ Tests/isPrimeNumber.test.ts
@@ -1,6 +1,22 @@
 import { isPrimeNumber } from '../Functions/isPrimeNumber';
 
-test ('isPrimeNumber', () => {
-    expect(isPrimeNumber(2)).toBe(true);
-    expect(isPrimeNumber(1)).toBe(false);
+describe('isPrimeNumber', () => {
+    it('returns true for prime numbers', () => {
+        const primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+        primeNumbers.forEach(number => {
+            expect(isPrimeNumber(number)).toBe(true)
+        })
+    })
+
+    it('returns false for non-prime numbers', () => {
+        const nonPrimeNumbers = [1, 4, 6, 8, 9, 10, 12, 14, 15]
+        nonPrimeNumbers.forEach(number => {
+            expect(isPrimeNumber(number)).toBe(false)
+        })
+    })
+
+    it('returns false for negative numbers', () => {
+        expect(isPrimeNumber(-1)).toBe(false)
+        expect(isPrimeNumber(-3)).toBe(false)
+    })
 })
